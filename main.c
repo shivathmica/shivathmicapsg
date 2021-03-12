15e0fda21437eb77cef703448d9e156b113886a9
shivathmica <72804436+shivathmica@users.noreply.github.com>
2021-03-12 11:53:02 +0530

    Add files via upload

---
+++ main.c
@@ -0,0 +1,872 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+#include <conio.h>
+#include<ctype.h>
+#include <windows.h>
+#include <process.h>
+#define UP 72
+#define DOWN 80
+#define LEFT 75
+#define RIGHT 77
+int dummy=0;
+int board[10] = {2,2,2,2,2,2,2,2,2,2};
+int turn = 1,flag = 0;
+int player,comp;
+
+void menu();
+void go(int n);
+void start_game();
+void check_draw();
+void draw_board();
+void player_first();
+void put_X_O(char ch,int pos);
+COORD coord= {0,0};
+
+int length;
+int bend_no;
+int len;
+char key;
+void record();
+void load();
+int life;
+void Delay(long double);
+void Move();
+void Food();
+int Score();
+void Print();
+void gotoxy(int x, int y);
+void GotoXY(int x,int y);
+void Bend();
+void Boarder();
+void Down();
+void Left();
+void Up();
+void Right();
+void ExitGame();
+int Scoreonly();
+
+struct coordinate
+{
+    int x;
+    int y;
+    int direction;
+};
+
+typedef struct coordinate coordinate;
+
+coordinate head, bend[500],food,body[30];
+
+int main()
+{
+
+    char key;
+
+    Print();
+
+    system("cls");
+
+    load();
+
+    length=5;
+
+    head.x=25;
+
+    head.y=20;
+
+    head.direction=RIGHT;
+
+    Boarder();
+
+    Food(); //to generate food coordinates initially
+
+    life=3; //number of extra lives
+
+    bend[0]=head;
+
+    Move();   //initialing initial bend coordinate
+
+    return 0;
+
+}
+
+void Move()
+{
+    int a,i;
+
+    do
+    {
+
+        Food();
+        fflush(stdin);
+
+        len=0;
+
+        for(i=0; i<30; i++)
+
+        {
+
+            body[i].x=0;
+
+            body[i].y=0;
+
+            if(i==length)
+
+                break;
+
+        }
+
+        Delay(length);
+
+        Boarder();
+
+        if(head.direction==RIGHT)
+
+            Right();
+
+        else if(head.direction==LEFT)
+
+            Left();
+
+        else if(head.direction==DOWN)
+
+            Down();
+
+        else if(head.direction==UP)
+
+            Up();
+
+        ExitGame();
+
+    }
+    while(!kbhit());
+
+    a=getch();
+
+    if(a==27)
+
+    {
+
+        system("cls");
+
+        exit(0);
+
+    }
+    key=getch();
+
+    if((key==RIGHT&&head.direction!=LEFT&&head.direction!=RIGHT)||(key==LEFT&&head.direction!=RIGHT&&head.direction!=LEFT)||(key==UP&&head.direction!=DOWN&&head.direction!=UP)||(key==DOWN&&head.direction!=UP&&head.direction!=DOWN))
+
+    {
+
+        bend_no++;
+
+        bend[bend_no]=head;
+
+        head.direction=key;
+
+        if(key==UP)
+
+            head.y--;
+
+        if(key==DOWN)
+
+            head.y++;
+
+        if(key==RIGHT)
+
+            head.x++;
+
+        if(key==LEFT)
+
+            head.x--;
+
+        Move();
+
+    }
+
+    else if(key==27)
+
+    {
+
+        system("cls");
+
+        exit(0);
+
+    }
+
+    else
+
+    {
+
+        printf("\a");
+
+        Move();
+
+    }
+}
+
+void gotoxy(int x, int y)
+{
+
+    COORD coord;
+
+    coord.X = x;
+
+    coord.Y = y;
+
+    SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
+
+}
+void GotoXY(int x, int y)
+{
+    HANDLE a;
+    COORD b;
+    fflush(stdout);
+    b.X = x;
+    b.Y = y;
+    a = GetStdHandle(STD_OUTPUT_HANDLE);
+    SetConsoleCursorPosition(a,b);
+}
+void load()
+{
+    int row,col,r,c,q;
+    gotoxy(36,14);
+    printf("loading...");
+    gotoxy(30,15);
+    for(r=1; r<=20; r++)
+    {
+        for(q=0; q<=100000000; q++); //to display the character slowly
+        printf("%c",177);
+    }
+    getch();
+}
+void Down()
+{
+    int i;
+    for(i=0; i<=(head.y-bend[bend_no].y)&&len<length; i++)
+    {
+        GotoXY(head.x,head.y-i);
+        {
+            if(len==0)
+                printf("v");
+            else
+                printf("*");
+        }
+        body[len].x=head.x;
+        body[len].y=head.y-i;
+        len++;
+    }
+    Bend();
+    if(!kbhit())
+        head.y++;
+}
+void Delay(long double k)
+{
+    Score();
+    long double i;
+    for(i=0; i<=(10000000); i++);
+}
+void ExitGame()
+{
+    int i,check=0;
+    for(i=4; i<length; i++) //starts with 4 because it needs minimum 4 element to touch its own body
+    {
+        if(body[0].x==body[i].x&&body[0].y==body[i].y)
+        {
+            check++;    //check's value increases as the coordinates of head is equal to any other body coordinate
+        }
+        if(i==length||check!=0)
+            break;
+    }
+    if(head.x<=10||head.x>=70||head.y<=10||head.y>=30||check!=0)
+    {
+        life--;
+        if(life>0)
+        {
+            head.x=25;
+            head.y=20;
+            bend_no=0;
+            head.direction=RIGHT;
+            Move();
+
+        }
+        else
+        {
+            if(life==0)
+            {
+            system("cls");
+            printf("would you like to play \n1.random number\n2.tic tac toe \n3.quit the game\n");
+            int choice;
+            scanf("%d",&choice);
+            switch(choice)
+            {
+
+            case 1:
+
+system("cls");
+                int num, guess, tries = 0;int dummy=0;
+    srand(time(0)); /* seed random number generator */
+    num = rand() % 100 + 1; /* random number between 1 and 100 */
+
+    printf("Guess My Number Game\n\n");
+
+    do
+    {
+        printf("Enter a guess between 1 and 100 : ");
+        scanf("%d", &guess);
+        tries++;
+
+        if (guess > num)
+        {
+            printf("Too high!\n\n");
+        }
+        else if (guess < num)
+        {
+            printf("Too low!\n\n");
+        }
+        else
+        {
+            dummy++;
+            printf("\nCorrect! You got it in %d guesses!\n", tries);
+        }
+
+    }while (tries<7);
+    if(dummy!=0)
+    {
+            head.x=25;
+            head.y=20;
+            bend_no=0;
+            head.direction=RIGHT;
+            Move();
+    }
+    break;
+            case 2:
+                system("cls");
+                menu();
+                break;
+            case 3:
+                system("cls");
+                printf("quitting the game");
+            }
+            }
+            system("cls");
+            printf("All lives completed\nBetter Luck Next Time!!!\nPress any key to quit the game\n");
+            record();
+            exit(0);
+        }
+    }
+}
+
+void Food()
+{
+    if(head.x==food.x&&head.y==food.y)
+    {
+        length++;
+        time_t a;
+        a=time(0);
+        srand(a);
+        food.x=rand()%70;
+        if(food.x<=10)
+            food.x+=11;
+        food.y=rand()%30;
+        if(food.y<=10)
+
+            food.y+=11;
+    }
+    else if(food.x==0)/*to create food for the first time coz global variable are initialized with 0*/
+    {
+        food.x=rand()%70;
+        if(food.x<=10)
+            food.x+=11;
+        food.y=rand()%30;
+        if(food.y<=10)
+            food.y+=11;
+    }
+}
+void Left()
+{
+    int i;
+    for(i=0; i<=(bend[bend_no].x-head.x)&&len<length; i++)
+    {
+        GotoXY((head.x+i),head.y);
+        {
+            if(len==0)
+                printf("<");
+            else
+                printf("*");
+        }
+        body[len].x=head.x+i;
+        body[len].y=head.y;
+        len++;
+    }
+    Bend();
+    if(!kbhit())
+        head.x--;
+
+}
+void Right()
+{
+    int i;
+    for(i=0; i<=(head.x-bend[bend_no].x)&&len<length; i++)
+    {
+        //GotoXY((head.x-i),head.y);
+        body[len].x=head.x-i;
+        body[len].y=head.y;
+        GotoXY(body[len].x,body[len].y);
+        {
+            if(len==0)
+                printf(">");
+            else
+                printf("*");
+        }
+
+        len++;
+    }
+    Bend();
+    if(!kbhit())
+        head.x++;
+}
+void Bend()
+{
+    int i,j,diff;
+    for(i=bend_no; i>=0&&len<length; i--)
+    {
+        if(bend[i].x==bend[i-1].x)
+        {
+            diff=bend[i].y-bend[i-1].y;
+            if(diff<0)
+                for(j=1; j<=(-diff); j++)
+                {
+                    body[len].x=bend[i].x;
+                    body[len].y=bend[i].y+j;
+                    GotoXY(body[len].x,body[len].y);
+                    printf("*");
+                    len++;
+                    if(len==length)
+                        break;
+                }
+            else if(diff>0)
+                for(j=1; j<=diff; j++)
+                {
+                    /*GotoXY(bend[i].x,(bend[i].y-j));
+                    printf("*");*/
+                    body[len].x=bend[i].x;
+                    body[len].y=bend[i].y-j;
+                    GotoXY(body[len].x,body[len].y);
+                    printf("*");
+                    len++;
+                    if(len==length)
+                        break;
+                }
+        }
+        else if(bend[i].y==bend[i-1].y)
+        {
+            diff=bend[i].x-bend[i-1].x;
+            if(diff<0)
+                for(j=1; j<=(-diff)&&len<length; j++)
+                {
+                    /*GotoXY((bend[i].x+j),bend[i].y);
+                    printf("*");*/
+                    body[len].x=bend[i].x+j;
+                    body[len].y=bend[i].y;
+                    GotoXY(body[len].x,body[len].y);
+                    printf("*");
+                    len++;
+                    if(len==length)
+                        break;
+                }
+            else if(diff>0)
+                for(j=1; j<=diff&&len<length; j++)
+                {
+
+                    body[len].x=bend[i].x-j;
+                    body[len].y=bend[i].y;
+                    GotoXY(body[len].x,body[len].y);
+                    printf("*");
+                    len++;
+                    if(len==length)
+                        break;
+                }
+        }
+    }
+}
+void Boarder()
+{
+    system("cls");
+    int i;
+    GotoXY(food.x,food.y);   /*displaying food*/
+    printf("F");
+    for(i=10; i<71; i++)
+    {
+        GotoXY(i,10);
+        printf("!");
+        GotoXY(i,30);
+        printf("!");
+    }
+    for(i=10; i<31; i++)
+    {
+        GotoXY(10,i);
+        printf("!");
+        GotoXY(70,i);
+        printf("!");
+    }
+}
+void Print()
+{
+    //GotoXY(10,12);
+    printf("\tWelcome to the mini Snake game.(press any key to continue)\n");
+    getch();
+    system("cls");
+    printf("\tGame instructions:\n");
+    printf("\n-> Use arrow keys to move the snake.\n\n-> You will be provided foods at the several coordinates of the screen which you have to eat. Everytime you eat a food the length of the snake will be increased by 1 element and thus the score.\n\n-> Here you are provided with three lives. Your life will decrease as you hit the wall or snake's body.\n\n-> YOu can pause the game in its middle by pressing any key. To continue the paused game press any other key once again\n\n-> If you want to exit press esc. \n");
+    printf("\n\nPress any key to play game...");
+    if(getch()==27)
+        exit(0);
+}
+void record()
+{
+    char plname[20],nplname[20],cha,c;
+    int i,j,px;
+    FILE *info;
+    info=fopen("record.txt","a+");
+    getch();
+    system("cls");
+    printf("Enter your name\n");
+    scanf("%[^\n]",plname);
+    //************************
+    for(j=0; plname[j]!='\0'; j++) //to convert the first letter after space to capital
+    {
+        nplname[0]=toupper(plname[0]);
+        if(plname[j-1]==' ')
+        {
+            nplname[j]=toupper(plname[j]);
+            nplname[j-1]=plname[j-1];
+        }
+        else nplname[j]=plname[j];
+    }
+    nplname[j]='\0';
+    //*****************************
+    //sdfprintf(info,"\t\t\tPlayers List\n");
+    fprintf(info,"Player Name :%s\n",nplname);
+    //for date and time
+
+    time_t mytime;
+    mytime = time(NULL);
+    fprintf(info,"Played Date:%s",ctime(&mytime));
+    //**************************
+    fprintf(info,"Score:%d\n",px=Scoreonly());//call score to display score
+    //fprintf(info,"\nLevel:%d\n",10);//call level to display level
+    for(i=0; i<=50; i++)
+        fprintf(info,"%c",'_');
+    fprintf(info,"\n");
+    fclose(info);
+    printf("Want to see past records press 'y'\n");
+    cha=getch();
+    system("cls");
+    if(cha=='y')
+    {
+        info=fopen("record.txt","r");
+        do
+        {
+            putchar(c=getc(info));
+        }
+        while(c!=EOF);
+    }
+    fclose(info);
+}
+int Score()
+{
+    int score;
+    GotoXY(20,8);
+    score=length-5;
+    printf("SCORE : %d",(length-5));
+    score=length-5;
+    GotoXY(50,8);
+    printf("Life : %d",life);
+    return score;
+}
+int Scoreonly()
+{
+    int score=Score();
+    system("cls");
+    return score;
+}
+void Up()
+{
+    int i;
+    for(i=0; i<=(bend[bend_no].y-head.y)&&len<length; i++)
+    {
+        GotoXY(head.x,head.y+i);
+        {
+            if(len==0)
+                printf("^");
+            else
+                printf("*");
+        }
+        body[len].x=head.x;
+        body[len].y=head.y+i;
+        len++;
+    }
+    Bend();
+    if(!kbhit())
+        head.y--;
+}
+
+
+void menu()
+{
+    int choice;
+    system("cls");
+    printf("\n--------MENU--------");
+    printf("\n1 : Play with X");
+    printf("\n2 : Play with O");
+    printf("\n3 : Exit");
+    printf("\nEnter your choice:>");
+    scanf("%d",&choice);
+    turn = 1;
+    switch (choice)
+    {
+    case 1:
+        player = 1;
+        comp = 0;
+        player_first();
+        break;
+    case 2:
+        player = 0;
+        comp = 1;
+        start_game();
+        break;
+    case 3:
+        exit(1);
+    default:
+        menu();
+    }
+}
+
+int make2()
+{
+    if(board[5] == 2)
+        return 5;
+    if(board[2] == 2)
+        return 2;
+    if(board[4] == 2)
+        return 4;
+    if(board[6] == 2)
+        return 6;
+    if(board[8] == 2)
+        return 8;
+    return 0;
+}
+
+int make4()
+{
+    if(board[1] == 2)
+        return 1;
+    if(board[3] == 2)
+        return 3;
+    if(board[7] == 2)
+        return 7;
+    if(board[9] == 2)
+        return 9;
+    return 0;
+}
+
+int posswin(int p)
+{
+// p==1 then X   p==0  then  O
+    int i;
+    int check_val,pos;
+
+    if(p == 1)
+        check_val = 18;
+    else
+        check_val = 50;
+
+    i = 1;
+    while(i<=9)//row check
+    {
+        if(board[i] * board[i+1] * board[i+2] == check_val)
+        {
+            if(board[i] == 2)
+                return i;
+            if(board[i+1] == 2)
+                return i+1;
+            if(board[i+2] == 2)
+                return i+2;
+        }
+        i+=3;
+    }
+
+    i = 1;
+    while(i<=3)//column check
+    {
+        if(board[i] * board[i+3] * board[i+6] == check_val)
+        {
+            if(board[i] == 2)
+                return i;
+            if(board[i+3] == 2)
+                return i+3;
+            if(board[i+6] == 2)
+                return i+6;
+        }
+        i++;
+    }
+
+    if(board[1] * board[5] * board[9] == check_val)
+    {
+        if(board[1] == 2)
+            return 1;
+        if(board[5] == 2)
+            return 5;
+        if(board[9] == 2)
+            return 9;
+    }
+
+    if(board[3] * board[5] * board[7] == check_val)
+    {
+        if(board[3] == 2)
+            return 3;
+        if(board[5] == 2)
+            return 5;
+        if(board[7] == 2)
+            return 7;
+    }
+    return 0;
+}
+
+void go(int n)
+{
+    if(turn % 2)
+        board[n] = 3;
+    else
+        board[n] = 5;
+    turn++;
+}
+
+void player_first()
+{
+    int pos;
+
+    check_draw();
+    draw_board();
+    gotoxy(30,18);
+    printf("Your Turn :> ");
+    scanf("%d",&pos);
+
+    if(board[pos] != 2)
+        player_first();
+
+    if(pos == posswin(player))
+    {
+        go(pos);
+        draw_board();
+        gotoxy(30,20);
+        //textcolor(128+RED);
+        printf("Player Wins");dummy=1;
+        getch();
+
+    }
+
+    go(pos);
+    draw_board();
+    start_game();
+}
+
+void start_game()
+{
+// p==1 then X   p==0  then  O
+    if(posswin(comp))
+    {
+        go(posswin(comp));
+        flag = 1;
+    }
+    else if(posswin(player))
+        go(posswin(player));
+    else if(make2())
+        go(make2());
+    else
+        go(make4());
+    draw_board();
+
+    if(flag)
+    {
+        gotoxy(30,20);
+        //textcolor(128+RED);
+        printf("Computer wins");
+        getch();
+    }
+    else
+        player_first();
+}
+
+void check_draw()
+{
+    if(turn > 9)
+    {
+        gotoxy(30,20);
+
+        printf("Game Draw");dummy=1;
+        head.x=25;
+            head.y=20;
+            bend_no=0;
+            head.direction=RIGHT;
+            Move();
+
+
+    }
+}
+
+void draw_board()
+{
+    int j;
+
+    for(j=9; j<17; j++)
+    {
+        gotoxy(35,j);
+        printf("|       |");
+    }
+    gotoxy(28,11);
+    printf("-----------------------");
+    gotoxy(28,14);
+    printf("-----------------------");
+
+    for(j=1; j<10; j++)
+    {
+        if(board[j] == 3)
+            put_X_O('X',j);
+        else if(board[j] == 5)
+            put_X_O('O',j);
+    }
+}
+
+void put_X_O(char ch,int pos)
+{
+    int m;
+    int x = 31, y = 10;
+
+    m = pos;
+
+    if(m > 3)
+    {
+        while(m > 3)
+        {
+            y += 3;
+            m -= 3;
+        }
+    }
+    if(pos % 3 == 0)
+        x += 16;
+    else
+    {
+        pos %= 3;
+        pos--;
+        while(pos)
+        {
+            x+=8;
+            pos--;
+        }
+    }
+    gotoxy(x,y);
+    printf("%c",ch);
+}
+
